791e90594c1b25a69c024871a20dbfc561b109d2
Miro Radenovic <miro.radenovic@dev4side.com>
2015-06-04 12:22:17 +0200

    updated js

--- taskList.js
+++ taskList.js
@@ -1,17 +1,14 @@
 if (Meteor.isClient) {
   // counter starts at 0
-  Session.setDefault('counter', 0);
-
   Template.hello.helpers({
-    counter: function () {
-      return Session.get('counter');
+    tasks: function () {
+      return Tasks.find({});
     }
   });
 
   Template.hello.events({
     'click button': function () {
-      // increment the counter when button is clicked
-      Session.set('counter', Session.get('counter') + 1);
+		Tasks.insert({text: $("#newTask").val()});
     }
   });
 }
